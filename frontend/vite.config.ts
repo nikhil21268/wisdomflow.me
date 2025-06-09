b74e40f5e76449e10480a77e36a82c27931f4ede
Nikhil Suri <nikhil21268@iiitd.ac.in>
2025-06-09 21:13:02 +0530

    chore: add vite react plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
+  root: 'public',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/src': resolve(__dirname, 'src')
+    }
+  },
   build: {
-    outDir: 'build'
+    outDir: '../build'
   }
 });
